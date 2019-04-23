64a5b00e2ae4c944b4029c924e7babdaa308cc80
jfkinslow <killajk2201@gmail.com>
2019-04-23 17:16:53 -0500

    Got myself started. This was built using php so that I could make use of my in house templating framework that I built a month ago.

---
+++ system/views/stubs/footer.php
@@ -0,0 +1,10 @@
+    <div class="row">
+        <div class="col-sm-12">
+            <address>
+                &copy; 2019 Joshua F. Kinslow
+            </address>
+        </div>
+    </div>
+</div>
+</body>
+</html>
